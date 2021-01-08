f5a9a3652867c959ca354250d716a256d38c57a9
sunswan <sunswanleva@gmail.com>
2021-01-08 14:50:02 +0200

    pro

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ ReactDOM.render(
   </React.StrictMode>
   </BrowserRouter>,
   document.getElementById('root')
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
